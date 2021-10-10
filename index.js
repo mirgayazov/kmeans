e94c84f702bb048e45efdb1536f215de2e4a7cbb
MKamill <mirgayazow2014@yandex.ru>
2021-10-10 12:07:50 +0300

    finally

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
 const fs = require('fs')
 
-fs.readFile('data.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  let arr = data.split('\r\n')
-  arr = arr.map(str => {
-    return (
-      [
-      Number(str.split('\t')[2]),
-      Number(str.split('\t')[3]),
-      str.split('\t')[4]]
-    )
-    // return (
-    //   [Number(str.split('\t')[0]),
-    //   Number(str.split('\t')[1]),
-    //   Number(str.split('\t')[2]),
-    //   Number(str.split('\t')[3]),
-    //   str.split('\t')[4]]
-    // )
-  })
-  fs.writeFile('3.txt', JSON.stringify(arr), err => {
+fs.readFile('fishersIrises.txt', 'utf8', (err, data) => {
     if (err) {
-      console.error(err)
-      return
+        console.error(err)
+        return
     }
-    //file written successfully
-  })
+    let arr = data.split('\r\n')
+    arr = arr.map(str => {
+        return (
+            [
+                Number(str.split('\t')[0]),
+                Number(str.split('\t')[1]),
+                Number(str.split('\t')[2]),
+                Number(str.split('\t')[3]),
+                str.split('\t')[4]
+            ]
+        )
+    })
+    fs.writeFile('fishersIrises.js', 'let fishersIrises = ' +
+        JSON.stringify(arr) + '\nmodule.exports = fishersIrises', err => {
+            if (err) {
+                console.error(err)
+                return
+            }
+        }
+    )
 })
